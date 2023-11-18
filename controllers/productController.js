bd826441bccdbd4646d8cd76759508a72ba61c76
Filip Maćkowiak <Fil.Mackowiak@gmail.com>
2023-11-19 00:48:16 +0100

    Simple CRUD

--- controllers/productController.js
+++ controllers/productController.js
@@ -18,7 +18,7 @@ const setProduct = asyncHandler(async (req, res) => {
     throw new Error("Missing name or price");
   }
 
-  const product = await Product.create(req.body)
+  const product = await Product.create(req.body);
 
   res.status(200).json(product);
 });
@@ -26,13 +26,33 @@ const setProduct = asyncHandler(async (req, res) => {
 // @desc    Update product
 // @route   PUT /products/:id
 const updateProduct = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "PUT" });
+  const providedId = req.params.id;
+  const product = await Product.findById(providedId);
+
+  if (!product) {
+    res.status(400);
+    throw new Error("Product not found");
+  }
+
+  const updatedProduct = await Product.findByIdAndUpdate(providedId, req.body, {new: true});
+
+  res.status(200).json(updatedProduct);
 });
 
 // @desc    Delete product
 // @route   DELETE /products/:id
 const deleteProduct = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "DELETE" });
+  const providedId = req.params.id;
+  const product = await Product.findById(providedId);
+
+  if (!product) {
+    res.status(400);
+    throw new Error("Product not found");
+  }
+
+  await Product.deleteOne();
+
+  res.status(200).json({id: providedId});
 });
 
 module.exports = {
